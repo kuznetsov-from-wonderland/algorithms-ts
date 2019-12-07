e111be1570e4f9411d34e8566bbeb24b5ecd5cce
Ilya Kuznetsov <kromsik@icloud.com>
2019-12-07 13:02:14 +0300

    add bubble sort

--- src/bubble-sort/bubble-sort.test.ts
+++ src/bubble-sort/bubble-sort.test.ts
@@ -1,9 +1,15 @@
-import { swap } from './bubble-sort'
+import { swap, bubbleSort } from './bubble-sort'
 
 describe('swap ES6', () => {
   test('should swap array elements', () => {
     const arr = [1, 5, 16, 20, 's']
     swap(arr, 0, 4)
     expect(arr).toEqual(['s', 5, 16, 20, 1])
+  })
+  test('bubblesort', () => {
+    const arr = [1, 5, 16, 20, 2, 0, -1, -100, 19, 19]
+    const sortedArr = bubbleSort(arr)
+    expect(sortedArr.length).toEqual(arr.length)
+    expect(sortedArr).toEqual([-100, -1, 0, 1, 2, 5, 16, 19, 19, 20])
   })
 })
--- src/bubble-sort/bubble-sort.ts
+++ src/bubble-sort/bubble-sort.ts
@@ -1,9 +1,17 @@
-const bubbleSort = <T>(arr: T[]): T[] => {
-  return arr
-}
-
 const swap = <T>(arr: T[], idx1: number, idx2: number) => {
   ;[arr[idx2], arr[idx1]] = [arr[idx1], arr[idx2]]
+}
+
+const bubbleSort = <T>(arr: T[]): T[] => {
+  const shallowCopy = [...arr]
+  for (let i = shallowCopy.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (shallowCopy[j] > shallowCopy[j + 1]) {
+        swap(shallowCopy, j, j + 1)
+      }
+    }
+  }
+  return shallowCopy
 }
 
 export { swap, bubbleSort }
