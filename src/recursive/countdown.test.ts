d5a84ff690c5649d8cef58767edd32608f43988f
Ilya Kuznetsov <kromsik@icloud.com>
2019-12-08 12:32:10 +0300

    update countdown tests

--- src/recursive/countdown.test.ts
+++ src/recursive/countdown.test.ts
@@ -1,9 +1,18 @@
 import { countdownIterative, countdownRecursive } from './countdown'
 
+const spy = jest.spyOn(console, 'log').mockImplementation()
+
+afterAll(() => {
+  spy.mockRestore()
+})
+
 test('should countdown', () => {
   countdownIterative(5)
+  expect(spy).toHaveBeenCalledTimes(6)
+  spy.mockReset()
 })
 
 test('should countdown', () => {
   countdownRecursive(5)
+  expect(spy).toHaveBeenCalledTimes(6)
 })
